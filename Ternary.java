8ea61cba9042edda4b43518fe41c39d26a1d607e
AKASH SANTOSH CHAUDHARI <65148480+akash2941@users.noreply.github.com>
2020-09-17 23:25:22 +0530

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Ternary{
+		public static void main(String args []){
+		Scanner sc =new Scanner(System.in);
+		System.out.println("enter three number");
+	    int a=sc.nextInt();
+        int b=sc.nextInt();
+		int c=sc.nextInt();
+		String ans=(a>b)?((a>c)?a+" is greater ":c+" is greater"):((b>c)?b+" is greater":c+" is greater");
+		System.out.println(ans);
+
+	}
+}
